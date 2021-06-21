060b30b0d80a5c5751170b8fcfe16e47880a43f4
Bruno Marian <bruno.marian.info@gmail.com>
2021-06-21 15:26:15 -0300

    Ajustes de pesquisa de cliente

--- Viwer/vwseach_client.py
+++ Viwer/vwseach_client.py
@@ -1,3 +1,5 @@
+import wx
+
 from Viwer import noname
 from Service.svClient import count_client
 from DataBase.conection import Client
@@ -17,13 +19,44 @@ class SeachClient(noname.seach_client):
         self._search = value
 
     def OnbtPesquisaClick(self, event):
-        client = Client.select(Client.nome, Client.id, Client.cpf).order_by(Client.id.asc())
-        for row in range(count_client()):
-            if row > 14:
-                self.grid_client.AppendRows(1)
-            self.grid_client.SetCellValue(row, 1, client[row].nome)
-            self.grid_client.SetCellValue(row, 0, str(client[row].id))
-            self.grid_client.SetCellValue(row, 2, client[row].cpf)
+        if self.m_comboBox2.Selection == -1:
+            self.grid_client.ClearGrid()
+            client = Client.select(Client.nome, Client.id, Client.cpf).order_by(Client.id.asc())
+            for row in range(count_client()):
+                if row > 14:
+                    self.grid_client.AppendRows(1)
+                self.grid_client.SetCellValue(row, 1, client[row].nome)
+                self.grid_client.SetCellValue(row, 0, str(client[row].id))
+                self.grid_client.SetCellValue(row, 2, client[row].cpf)
+
+        elif self.m_comboBox2.Selection == 0:
+            self.grid_client.ClearGrid()
+            try:
+                search_name = Client.select(Client.nome, Client.id, Client.cpf).where(Client.nome.contains(
+                    self.ed_pesquisa.GetValue()))
+                for row in range(count_client()):
+                    if row > 14:
+                        self.grid_client.AppendRows(1)
+                    self.grid_client.SetCellValue(row, 1, search_name[row].nome)
+                    self.grid_client.SetCellValue(row, 0, str(search_name[row].id))
+                    self.grid_client.SetCellValue(row, 2, search_name[row].cpf)
+            except IndexError:
+                wx.SafeShowMessage('Erro!', 'Pesquisa não encontrada!')
+
+        elif self.m_comboBox2.Selection == 1:
+            self.grid_client.ClearGrid()
+            try:
+                search_cpf = Client.select(Client.nome, Client.id, Client.cpf).where(Client.cpf.contains(
+                    self.ed_pesquisa.GetValue()))
+                for row in range(count_client()):
+                    if row > 14:
+                        self.grid_client.AppendRows(1)
+                    self.grid_client.SetCellValue(row, 1, search_cpf[row].nome)
+                    self.grid_client.SetCellValue(row, 0, str(search_cpf[row].id))
+                    self.grid_client.SetCellValue(row, 2, search_cpf[row].cpf)
+            except IndexError:
+                wx.SafeShowMessage('Erro!', 'Pesquisa não encontrada!')
+
         event.Skip()
 
     def OnDBClickGrid(self, event):
